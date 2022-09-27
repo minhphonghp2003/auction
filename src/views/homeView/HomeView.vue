dd5f9c161854ecf444e73b02797af0d75b482c94
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-27 10:27:11 +0700

    n

--- src/views/homeView/HomeView.vue
+++ src/views/homeView/HomeView.vue
@@ -4,34 +4,46 @@ import axios from "axios";
 import ProductView from "../ProductView.vue";
 import HeroView from "./HeroView.vue";
 import { ContentLoader } from "vue-content-loader";
+import MostWatchedView from "./MostWatchedView.vue";
 
 import { Buffer } from "buffer";
 
 let product = ref([]);
 let subproduct = ref([]);
 let section = ref("new");
+let mostWatched = ref({})
+
+function compareNumbers(a, b) {
+  return a.b_count - b.b_count;
+}
 
 axios
-  .get("https://ecommerce-r6l7.onrender.com/product/all?page=1")
+  .get("https://ecommerce-r6l7.onrender.com/product/all")
   .then((data) => (product.value = data.data))
   .then((data) => {
     product.value.forEach((element) => {
       element.image = Buffer.from(element.image).toString("base64");
     });
   })
-  .then((data) => (subproduct.value = product.value));
+  .then(
+    (data) => (subproduct.value = JSON.parse(JSON.stringify(product.value)))
+  );
 
 let renderMost = () => {
-  product.value = product.value.filter((ele) => ele.status == "active");
+  product.value.sort(compareNumbers).reverse();
+  product.value = product.value.splice(0, product.value.length / 2);
+
   section.value = "most";
+  mostWatched.value = product.value[0]
 };
 let renderNew = () => {
-  product.value = subproduct.value;
+  product.value = JSON.parse(JSON.stringify(subproduct.value));
   section.value = "new";
 };
 let done = () => {
   return product.value.length;
 };
+
 </script>
 
 <template>
@@ -45,22 +57,24 @@ let done = () => {
           Most Watched
         </li>
       </ul>
-      <content-loader v-if="!done()"
-      viewBox="0 0 400 150"
-    :speed="2"
-    primaryColor="#f3f3f3"
-    secondaryColor="#ecebeb"
-  >
-    <circle cx="10" cy="20" r="8" /> 
-    <circle cx="10" cy="50" r="8" /> 
-    <circle cx="10" cy="80" r="8" /> 
-    <circle cx="10" cy="110" r="8" /> 
-    <rect x="34" y="13" rx="0" ry="0" width="504" height="103" />
+      <content-loader
+        v-if="!done()"
+        viewBox="0 0 400 150"
+        :speed="2"
+        primaryColor="#f3f3f3"
+        secondaryColor="#ecebeb"
+      >
+        <circle cx="10" cy="20" r="8" />
+        <circle cx="10" cy="50" r="8" />
+        <circle cx="10" cy="80" r="8" />
+        <circle cx="10" cy="110" r="8" />
+        <rect x="34" y="13" rx="0" ry="0" width="504" height="103" />
       </content-loader>
     </div>
   </div>
 
   <ProductView :key="product" :product="product"></ProductView>
+  <MostWatchedView v-if="done()" v-bind="mostWatched"  ></MostWatchedView>
 </template>
 <style scoped>
 </style>
---
+++ src/views/homeView/MostWatchedView.vue
@@ -0,0 +1,57 @@
+
+<script setup>
+defineProps(['b_count','name','category','date_end','image'])
+</script>
+
+<template>
+   <section class="categories spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="categories__text">
+                        <h2>Hot <br /> <span>{{cate}}</span> <br /></h2>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="categories__hot__deal">
+                        <img v-bind:src="'data:image/jpeg;base64,'+image" alt="image">
+                        <div class="hot__deal__sticker">
+                            <span>Hot deal</span>
+                            <h5>{{b_count}} bidders</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 offset-lg-1">
+                    <div class="categories__deal__countdown">
+                        <span>Deal Of The Day</span>
+                        <h2>{{name}}</h2>
+                        <div class="categories__deal__countdown__timer" id="countdown">
+                            <div class="cd-item">
+                                <span>{{date_end}}</span>
+                                <p>Days</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>1</span>
+                                <p>Hours</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>50</span>
+                                <p>Minutes</p>
+                            </div>
+                            <div class="cd-item">
+                                <span>18</span>
+                                <p>Seconds</p>
+                            </div>
+                        </div>
+                        <a href="#" class="primary-btn">Shop now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+</template>
+
+<style>
+
+</style>
