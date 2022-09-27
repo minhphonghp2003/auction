aa1e1409a02641937585cfca3cb131b9436b285b
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-27 20:49:37 +0700

    n

--- src/views/authentication/AuthView.vue
+++ src/views/authentication/AuthView.vue
@@ -1,45 +1,88 @@
 
 <script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import RegisterView from './RegisterView.vue'
+
+let loginComp = ref(true)
+let swapComp = () =>{
+  loginComp.value = !loginComp.value
+}
+let username = ref('')
+let password = ref('')
+let isUsername = ref(true)
+let isPassword = ref(true)
+
+let checkNoInput = () =>{
+  if(!username.value){
+    isUsername.value = !isUsername.value
+    return true
+  }
+  if(!password.value){
+    isPassword.value = !isPassword.value
+    return true
+  }
+}
+
+let login = async() =>{
+  if(checkNoInput()){
+    return
+  }
+  let credential = {username:username.value, password:password.value}
+  try {
+    let res = await axios.post("https://ecommerce-r6l7.onrender.com/user/login",credential)
+    console.log(res.data);
+    
+  } catch (error) {
+   console.log(error); 
+  }
+
+}
+
 </script>
 
 <template>
-  <div class="limiter">
+  <RegisterView v-if="!loginComp"></RegisterView>
+  <div v-if="loginComp" class="limiter">
     <div
       class="container-login100"
       style="background-image: url('/src/assets/auth/images/bg-01.jpg')"
     >
       <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54" style="margin:150px">
-        <form class="login100-form validate-form">
           <span class="login100-form-title p-b-49"> Login </span>
 
           <div
+      
             class="wrap-input100 validate-input m-b-23"
-            data-validate="Username is reauired"
           >
             <span class="label-input100">Username</span>
             <input
               class="input100"
               type="text"
-              name="username"
+            name="username"
               placeholder="Type your username"
+              v-model="username"
             />
             <span class="focus-input100" data-symbol="&#xf206;"></span>
           </div>
-
+          <p  v-if="!isUsername" style="color:red">Username is require</p>
           <div
+       
             class="wrap-input100 validate-input"
-            data-validate="Password is required"
           >
             <span class="label-input100">Password</span>
             <input
               class="input100"
               type="password"
-              name="pass"
+          
+            name="password"
               placeholder="Type your password"
+              v-model="password"
             />
             <span class="focus-input100" data-symbol="&#xf190;"></span>
           </div>
 
+          <p  v-if="!isPassword" style="color:red">Password is require</p>
           <div class="text-right p-t-8 p-b-31">
             <a href="#"> Forgot password? </a>
           </div>
@@ -47,9 +90,11 @@
           <div class="container-login100-form-btn">
             <div class="wrap-login100-form-btn">
               <div class="login100-form-bgbtn"></div>
-              <button class="login100-form-btn">Login</button>
+              <!-- <button>login</button> -->
+              <button class="login100-form-btn" @click="login">Login</button>
             </div>
           </div>
+          
 
           <div class="txt1 text-center p-t-54 p-b-20">
             <span> Or Sign Up Using </span>
@@ -72,9 +117,8 @@
           <div class="flex-col-c p-t-155">
             <span class="txt1 p-b-17"> Or Sign Up Using </span>
 
-            <a href="#" class="txt2"> Sign Up </a>
+            <a href="#" @click="swapComp" class="txt2"> Sign Up </a>
           </div>
-        </form>
       </div>
     </div>
   </div>
---
+++ src/views/authentication/LoginView.vue
@@ -0,0 +1,99 @@
+
+<script setup>
+import { ref } from "@vue/reactivity";
+  let username = ref('')
+  let password = ref('')
+</script>
+
+<template>
+  <div class="limiter">
+    <div
+      class="container-login100"
+      style="background-image: url('/src/assets/auth/images/bg-01.jpg')"
+    >
+      <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54" style="margin:150px">
+        <form class="login100-form validate-form">
+          <span class="login100-form-title p-b-49"> Login </span>
+
+          <div
+            class="wrap-input100 validate-input m-b-23"
+            data-validate="Username is reauired"
+          >
+            <span class="label-input100">Username</span>
+            <input
+              class="input100"
+              type="text"
+              name="username"
+              placeholder="Type your username"
+              v-model="username"
+            />
+            <span class="focus-input100" data-symbol="&#xf206;"></span>
+          </div>
+
+          <div
+            class="wrap-input100 validate-input"
+            data-validate="Password is required"
+          >
+            <span class="label-input100">Password</span>
+            <input
+              class="input100"
+              type="password"
+              name="pass"
+              placeholder="Type your password"
+            />
+            <span class="focus-input100" data-symbol="&#xf190;"></span>
+          </div>
+
+          <div class="text-right p-t-8 p-b-31">
+            <a href="#"> Forgot password? </a>
+          </div>
+
+          <div class="container-login100-form-btn">
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button class="login100-form-btn">Login</button>
+            </div>
+          </div>
+
+          <div class="txt1 text-center p-t-54 p-b-20">
+            <span> Or Sign Up Using </span>
+          </div>
+
+          <div class="flex-c-m">
+            <a href="#" class="login100-social-item bg1">
+              <i class="fa fa-facebook"></i>
+            </a>
+
+            <a href="#" class="login100-social-item bg2">
+              <i class="fa fa-twitter"></i>
+            </a>
+
+            <a href="#" class="login100-social-item bg3">
+              <i class="fa fa-google"></i>
+            </a>
+          </div>
+
+          <div class="flex-col-c p-t-155">
+            <span class="txt1 p-b-17"> Or Sign Up Using </span>
+
+            <a href="#" class="txt2"> Sign Up </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div id="dropDownSelect1"></div>
+</template>
+<style scoped>
+@import "../../assets/auth/css/util.css";
+@import "../../assets/auth/css/main.css";
+@import "../../assets/auth/vendor/bootstrap/css/bootstrap.min.css";
+@import "../../assets/auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+@import "../../assets/auth/fonts/iconic/css/material-design-iconic-font.min.css";
+@import "../../assets/auth/vendor/animate/animate.css";
+@import "../../assets/auth/vendor/css-hamburgers/hamburgers.min.css";
+@import "../../assets/auth/vendor/animsition/css/animsition.min.css";
+@import "../../assets/auth/vendor/select2/select2.min.css";
+@import "../../assets/auth/vendor/daterangepicker/daterangepicker.css";
+</style>
